3984db2d6d045566210613223931294b4a53dd16
mhufflep <mhufflep@student.21-school.ru>
2022-09-07 22:45:40 +0500

    Colleen updated

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-/* Author: mhufflep */
+/*
+    Author: mhufflep
+*/
 void display() {
-    const char *s = "#include <stdio.h>%c%c/* Author: mhufflep */%cvoid display() {%c    const char *s = %c%s%c;%c    printf(s, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    /* Hola, amigo! */%c    display();%c    return 0;%c}";
-    printf(s, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+    const char *s = "#include <stdio.h>%c%c/*%c    Author: mhufflep%c*/%cvoid display() {%c    const char *s = %c%s%c;%c    printf(s, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main() {%c    /*%c        Hola, amigo!%c    */%c    display();%c    return 0;%c}";
+    printf(s, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 
 int main() {
-    /* Hola, amigo! */
+    /*
+        Hola, amigo!
+    */
     display();
     return 0;
 }
