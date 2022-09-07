4534b21570996cc4e4db709add8a64d4e69c72cf
mhufflep <mhufflep@student.21-school.ru>
2022-09-08 01:09:15 +0300

    Ubuntu changes + Sully.js fix

--- Grace/C/Grace_v2.c
+++ Grace/C/Grace_v2.c
@@ -4,7 +4,7 @@
 /*
     Author: mhufflep
 */
-#define OPEN(x) int fd = open(x, 1538, 0666);
-#define DISPLAY() dprintf(fd, s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 34, 34, 10);
-#define MAIN() int main() { const char *s = "#include <fcntl.h>%c#include <stdio.h>%c%c/*%c    Author: mhufflep%c*/%c#define OPEN(x) int fd = open(x, 1538, 0666);%c#define DISPLAY() dprintf(fd, s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 34, 34, 10);%c#define MAIN() int main() { const char *s = %c%s%c; OPEN(%cGrace_kid.c%c); DISPLAY(); }%cMAIN()"; OPEN("Grace_kid.c"); DISPLAY(); }
+#define OPEN(x) int fd = open(x, O_CREAT | O_TRUNC | O_RDWR, 0666);
+#define PRINT() dprintf(fd, s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 34, 34, 10);
+#define MAIN() int main() { const char *s = "#include <fcntl.h>%c#include <stdio.h>%c%c/*%c    Author: mhufflep%c*/%c#define OPEN(x) int fd = open(x, O_CREAT | O_TRUNC | O_RDWR, 0666);%c#define PRINT() dprintf(fd, s, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 34, 34, 10);%c#define MAIN() int main() { const char *s = %c%s%c; OPEN(%cGrace_kid.c%c); PRINT(); }%cMAIN()"; OPEN("Grace_kid.c"); PRINT(); }
 MAIN()
--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -7,12 +7,12 @@
 
 int main() {
     int n = 5;
-    const char *s = "#include <fcntl.h>%c#include <stdio.h>%c#include <stdlib.h>%c%c#define NAME(buf, f) snprintf(buf, sizeof(buf), f, n - 1)%c#define COMP(buf, src, exe) snprintf(buf, sizeof(buf), %cclang %%s -o %%s%c, src, exe)%c%cint main() {%c    int n = %d;%c    const char *s = %c%s%c;%c    char src[50], exe[50], comp[50];%c    NAME(src, %c./Sully_%%d.c%c);%c    NAME(exe, %c./Sully_%%d%c);%c    COMP(comp, src, exe);%c    int fd = open(src, 1538, 0777);%c    dprintf(fd, s, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, n - 1, 10, 34, s, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10);%c    system(comp);%c    return (n > 0 ? system(exe) : 0);%c}";
+    const char *s = "#include <fcntl.h>%c#include <stdio.h>%c#include <stdlib.h>%c%c#define NAME(buf, f) snprintf(buf, sizeof(buf), f, n - 1)%c#define COMP(buf, src, exe) snprintf(buf, sizeof(buf), %cclang %%s -o %%s%c, src, exe)%c%cint main() {%c    int n = %d;%c    const char *s = %c%s%c;%c    char src[50], exe[50], comp[50];%c    NAME(src, %c./Sully_%%d.c%c);%c    NAME(exe, %c./Sully_%%d%c);%c    COMP(comp, src, exe);%c    int fd = open(src, O_CREAT | O_TRUNC | O_RDWR, 0777);%c    dprintf(fd, s, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, n - 1, 10, 34, s, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10);%c    system(comp);%c    return (n > 0 ? system(exe) : 0);%c}";
     char src[50], exe[50], comp[50];
     NAME(src, "./Sully_%d.c");
     NAME(exe, "./Sully_%d");
     COMP(comp, src, exe);
-    int fd = open(src, 1538, 0777);
+    int fd = open(src, O_CREAT | O_TRUNC | O_RDWR, 0777);
     dprintf(fd, s, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, n - 1, 10, 34, s, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10);
     system(comp);
     return (n > 0 ? system(exe) : 0);
