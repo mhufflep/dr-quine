1648646386d2f917b3f8236cd395dc436def5218
mhufflep <mhufflep@student.21-school.ru>
2022-09-07 19:20:27 +0500

    Sullyv2 added + v1 updated

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -7,7 +7,7 @@
 
 int main() {
     int n = 5;
-    const char *s = "#include <fcntl.h>%c#include <stdio.h>%c#include <stdlib.h>%c%c#define NAME(buf, f) snprintf(buf, sizeof(buf), f, n - 1)%c#define COMP(buf, src, exe) snprintf(buf, sizeof(buf), %cclang %%s -o %%s%c, src, exe)%c%cint main() {%c    int n = %d;%c    const char *s = %c%s%c;%c    char src[50], exe[50], comp[50];%c    NAME(src, %c./Sully_%%d.c%c);%c    NAME(exe, %c./Sully_%%d%c);%c    COMP(comp, src, exe);%c    int fd = open(src, 1538, 0777);%c    dprintf(fd, s, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, n - 1, 10, 34, s, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10);%c    system(comp);%c    return (n > 0 ? system(exe) : 0);%c}";
+    const char *s = "#include <fcntl.h>%c#include <stdio.h>%c#include <stdlib.h>%c%c#define NAME(buf, f) snprintf(buf, sizeof(buf), f, n - 1)%c#define COMP(buf, src, exe) snprintf(buf, sizeof(buf), %cclang %%s -o %%s%c, src, exe)%c%cint main() {%c    int n = %d;%c    const char *s = %c%s%c;%c    char src[50], exe[50], comp[50];%c    NAME(src, %c./Sully_%%d.c%c);%c    NAME(exe, %c./Sully_%%d%c);%c    COMP(comp, src, exe);%c    int fd = open(src, 1538, 0777);%c    dprintf(fd, s, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, n - 1, 10, 34, s, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10);%c    system(comp);%c    return (n - 1 > 0 ? system(exe) : 0);%c}";
     char src[50], exe[50], comp[50];
     NAME(src, "./Sully_%d.c");
     NAME(exe, "./Sully_%d");
@@ -15,5 +15,5 @@ int main() {
     int fd = open(src, 1538, 0777);
     dprintf(fd, s, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, n - 1, 10, 34, s, 34, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10);
     system(comp);
-    return (n > 0 ? system(exe) : 0);
+    return (n - 1 > 0 ? system(exe) : 0);
 }
