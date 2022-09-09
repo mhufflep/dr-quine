2cada5a2fe8db65c3ad3bdb92b7fea903a003aa9
mhufflep <mhufflep@student.21-school.ru>
2022-09-09 14:54:22 +0300

    grace fixed

--- Grace/JavaScript/Grace.js
+++ Grace/JavaScript/Grace.js
@@ -2,7 +2,7 @@
     Author: mhufflep
 */
 
-const child = "Grace_kid.js";
 const write = `require('fs').writeFile("Grace_kid.js", data(data.toString()), function(err) {});`;
-const data = (x) => `/*\n    Author: mhufflep\n*/\n\nconst child = \"${child}\";\nconst write = \`${write}\`;\nconst data = ${x};\neval(write);`;
-eval(write);
+const execute = (x) => { eval(x) };
+const data = (x) => `/*\n    Author: mhufflep\n*/\n\nconst write = \`${write}\`;\nconst execute = ${execute.toString()};\nconst data = ${x};\nexecute(write);`;
+execute(write);
